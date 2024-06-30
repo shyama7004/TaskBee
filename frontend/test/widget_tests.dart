5f8db7ddb7d6118c86d77f12405e9037a201d5b9
Skreg <85214856+shyama7004@users.noreply.github.com>
2024-07-01 04:22:28 +0530

    Create widget_tests.dart

---
+++ frontend/test/widget_tests.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// Placeholder for widget tests
+void main() {
+  testWidgets('Example widget test', (WidgetTester tester) async {
+    // Write widget tests here
+  });
+}
