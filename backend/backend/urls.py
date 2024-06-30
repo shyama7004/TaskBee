61bbd71ebd1b8a11910189f050c927bd8ffe0a91
Skreg <85214856+shyama7004@users.noreply.github.com>
2024-06-30 22:22:07 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,1 +1,8 @@
+# urls.py
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('task_manager.urls')),
+]
