8770510aee79ce9e0e8f2f5c44941a8d4e3fe75c
Skreg <85214856+shyama7004@users.noreply.github.com>
2024-06-30 22:23:35 +0530

    Delete backend/special directory

--- backend/special/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('task_manager.urls')),
-]
