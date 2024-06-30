44030849dce2734a5cc3a345f435b12a2bcf1cf3
Skreg <85214856+shyama7004@users.noreply.github.com>
2024-07-01 04:14:06 +0530

    Create example_screen.dart

---
+++ frontend/lib/src/screens/example_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Placeholder for a screen
+class ExampleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Example Screen')),
+      body: Center(child: Text('Hello, World!')),
+    );
+  }
+}
